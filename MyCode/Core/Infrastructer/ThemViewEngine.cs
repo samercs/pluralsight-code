c08f702627e9d2c7d163d5ebe423c264a9899cd8
samercs <samer_mail_2006@yahoo.com>
2017-09-07 22:06:47 +0300

    add them feature.

---
+++ MyCode/Core/Infrastructer/ThemViewEngine.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Mvc;
+
+namespace MyCode.Core.Infrastructer
+{
+    public class ThemViewEngine: RazorViewEngine
+    {
+        public ThemViewEngine(string themName)
+        {
+            ViewLocationFormats = new[]
+            {
+                "~/Views/Thems/"+themName+"/{1}/{0}.cshtml",
+                "~/Views/Thems/"+themName+"/Shared/{0}.cshtml"
+            };
+
+            PartialViewLocationFormats = new[]
+            {
+                "~/Views/Thems/"+themName+"/{1}/{0}.cshtml",
+                "~/Views/Thems/"+themName+"/Shared/{0}.cshtml"
+            };
+            AreaViewLocationFormats = new[]
+            {
+                "~/Areas/{2}/Views/Thems/"+themName+"/{1}/{0}.cshtml",
+                "~/Areas/{2}/Views/Thems/"+themName+"/Shared/{0}.cshtml"
+            };
+            AreaPartialViewLocationFormats = new[]
+            {
+                "~/Areas/{2}/Views/Thems/"+themName+"/{1}/{0}.cshtml",
+                "~/Areas/{2}/Views/Thems/"+themName+"/Shared/{0}.cshtml"
+            };
+        }
+        
+    }
+}
--- MyCode/Global.asax.cs
+++ MyCode/Global.asax.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 using MyCode.Core.Filters;
+using MyCode.Core.Infrastructer;
 
 namespace MyCode
 {
@@ -13,6 +15,11 @@ namespace MyCode
     {
         protected void Application_Start()
         {
+            if (!string.IsNullOrWhiteSpace(ConfigurationManager.AppSettings["ActiveThem"]))
+            {
+                ViewEngines.Engines.Insert(0, new ThemViewEngine(ConfigurationManager.AppSettings["ActiveThem"]));
+            }
+
             AreaRegistration.RegisterAllAreas();
             GlobalFilters.Filters.Add(new SessionFilter());
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
